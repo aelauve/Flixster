38594793ffae7dea316c27b489eeae6ba4ad5dad
aedwards4 <aedwards4@hawk.iit.edu>
2021-02-01 09:23:18 -0600

    Added Push Navigation features

--- Flixster/MovieViewController.swift
+++ Flixster/MovieViewController.swift
@@ -67,14 +67,25 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         return cell
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie
+        let detailsVC = segue.destination as! MovieDetailsViewController
+        detailsVC.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
 
 }
